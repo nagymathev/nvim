c336f262db7bcba2ba50a5bcfbac5a63d29a6b87
nagymathev <nagymathev@gmail.com>
2024-12-09 17:14:39 +0100

    enable smartindent, then disable treesitter indentation for html, as it produces horsecrap

--- init.lua
+++ init.lua
@@ -104,6 +104,8 @@ vim.opt.number = true
 --  Experiment for yourself to see if you like it!
 vim.opt.relativenumber = true
 
+vim.opt.smartindent = true
+
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
 
